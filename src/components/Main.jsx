9fe9e28fce6ed39c67138f7931137fb6011be68c
olavo-goes <olavo.goes@estudante.ifms.edu.br>
2024-03-13 19:02:32 -0400

    correção.
    correção

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+export default function Main (){
+    const [nome, setNome] = useState("");
+    return(
+        <Main>
+
+           <input type="text" nome="nome" id="" onChange={(event) => setNome(event.target.value)} />
+
+        </Main>
+    )
+}
