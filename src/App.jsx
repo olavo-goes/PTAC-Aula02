1115a1ade97b2d23cc28aee1b19b7b9f027d8754
olavo-goes <olavo.goes@estudante.ifms.edu.br>
2024-03-13 21:01:12 -0400

    tentativa de correção
    tentativa de  correção

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Footer from  "./components/Footer";
 import Header from  "./components/Header";
-import Main from  "./components/Main";
+
 import "./globals.css";
 
 export default function App() {
@@ -8,7 +8,7 @@ export default function App() {
   return(
 <div>
   <Header/>
-  <Main/>
+
   <Footer/>
 </div>
 );
