53fc78b9468b8a5c5e7f36476ec274387d4045a7
olavo-goes <olavo.goes@estudante.ifms.edu.br>
2024-03-27 20:01:01 -0400

    atividade 07/03/2024
    atividade feita

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,20 +6,22 @@ export default function Main(){
 const [nome, setNome] = useState("");
 const [telefone, setTelefone] = useState("");
 const [listaContatos,setContatos] = useState([]);                    // estado inicial para uma lista
+const [Numero,setNumero] = useState([]);    
 
 const registrar = (event) => {
     event.preventDefault();  // serve para previnir o envio do formulario, ou seja não vai mais enviar o formulario apenas salvas as informações.
     alert("deu certo");
     setContatos([...listaContatos,{
         nomeSalvo: nome,                                      // coloca na lista os usuarios e salva o nome 
-        telefone: telefone                                    // 
+        telefone: telefone,    
+        numeroSalvo: Numero
     } ]); // modifica a lista e adiciona objetos.
 
 }
+
+
+
 console.table(listaContatos);                       // exibe as informações em formato de tabela.
-
-
-
 
     return(
         <main>
@@ -33,8 +35,27 @@ console.table(listaContatos);                       // exibe as informações em
         onChange={(event) => setNome(event.target.value)}
         />
         {nome}
-        
- 
+
+
+
+<input
+type="nunber"
+nome="numero"
+id="Numero"
+value={Numero}
+onChange={(event) => setNumero(event.target.value)}
+/>
+{Numero}
+
+<button type="submit">
+            <p>Numero</p>
+        </button>
+
+
+
+
+
+
     <label htmlFor ="telefone">Telefone:</label>
     
         <input
